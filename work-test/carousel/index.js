0db38e3dbe38426949492f60615444d8d174a2b9
Luxame <asd869zxc@hotmail.com.tw>
2016-09-22 02:17:06 +0800

    作業完成95%

---
+++ work-test/carousel/index.js
@@ -0,0 +1,27 @@
+function bigImg(x) {
+    x.style.height = "660px";
+    x.style.width = "990px";
+}
+
+function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "400px";
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
